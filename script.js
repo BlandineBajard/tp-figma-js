962981015715576cbb24eb1d9c9203010063821f
Blandine Bajard <83599148+BlandineBajard@users.noreply.github.com>
2021-10-18 12:34:36 +0200

    modif script

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ fetch(API_URL)
         MEMBER.innerHTML +=`
         <div class = "fiche">
         <img src="${PERSONNE.avatar}" alt="Photo d'identité">
-        <p class= "nom">${PERSONNE.last_name}</p>
-        <p class="prenom">${PERSONNE.first_name}</p>
+        <p class= "nom">${PERSONNE.last_name} + ${PERSONNE.first_name}</p>
+        <!--<p class="prenom">${PERSONNE.first_name}</p>-->
         <p class ="identifiant">${PERSONNE.id}</p>
         <p class = "email">${PERSONNE.email}</p>
         </div>
